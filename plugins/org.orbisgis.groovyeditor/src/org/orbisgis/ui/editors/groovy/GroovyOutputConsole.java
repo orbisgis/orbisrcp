faa4b46de2743aff965a5f90875471a359d80cbd
Palominos Sylvain <sylvain.palominos@univ-ubs.fr>
2022-03-14 15:23:31 +0100

    Fix null pointer exception.

--- plugins/org.orbisgis.groovyeditor/src/org/orbisgis/ui/editors/groovy/GroovyOutputConsole.java
+++ plugins/org.orbisgis.groovyeditor/src/org/orbisgis/ui/editors/groovy/GroovyOutputConsole.java
@@ -44,9 +44,9 @@ public class GroovyOutputConsole extends ViewPart implements DBRProcessControlle
 {
 	static Composite group = null;
 	static ToolbarButton button = null;
-	
+
 	public static final String VIEW_ID = "org.jkiss.dbeaver.groovy.output.clear";
-    public static final String CLEAR_CMD_ID = "org.orbisgis.ui.editors.groovy.clearOutput";
+	public static final String CLEAR_CMD_ID = "org.orbisgis.ui.editors.groovy.clearOutput";
 
 	@Override
 	public DBRProcessDescriptor getProcessDescriptor() {
@@ -62,11 +62,11 @@ public class GroovyOutputConsole extends ViewPart implements DBRProcessControlle
 	@Override
 	public void createPartControl(Composite parent) {
 		group = UIUtils.createPlaceholder(parent, 1);
-        group.setLayout(new FillLayout());
+		group.setLayout(new FillLayout());
 		GroovyConsoleContent.initialize(group);
 		createDeleteOutputButton(group);
 	}
-	
+
 	@Override
 	public void setFocus() {
 		// TODO Auto-generated method stub
@@ -80,15 +80,13 @@ public class GroovyOutputConsole extends ViewPart implements DBRProcessControlle
 	private void createDeleteOutputButton(Composite group) {
 		final MenuManager menuMgr = new MenuManager();
 		menuMgr.setRemoveAllWhenShown(true);
-        menuMgr.addMenuListener(manager -> {
-            manager.add(ActionUtils.makeCommandContribution(getSite(), CLEAR_CMD_ID));
-        });
-        group.setMenu(menuMgr.createContextMenu(group));
+		menuMgr.addMenuListener(manager -> manager.add(ActionUtils.makeCommandContribution(getSite(), CLEAR_CMD_ID)));
+		group.setMenu(menuMgr.createContextMenu(group));
 	}
 
 
 	public static class GroovyConsoleContent{
-	
+
 		static StyledText text = null;
 		static Display display = null;
 		static StyleRange style1 = null;
@@ -101,23 +99,20 @@ public class GroovyOutputConsole extends ViewPart implements DBRProcessControlle
 		 * @param group A given composite
 		 */
 		public static void initialize(Composite group) {
-	    	text = new StyledText(group, SWT.HORIZONTAL | SWT.READ_ONLY | SWT.V_SCROLL);
-	    	display = PlatformUI.getWorkbench().getDisplay();
-	    	text.setLayoutData(new GridData());
+			text = new StyledText(group, SWT.HORIZONTAL | SWT.READ_ONLY | SWT.V_SCROLL);
+			display = PlatformUI.getWorkbench().getDisplay();
+			text.setLayoutData(new GridData());
 		}
-		
+
 		/**
 		 * Write the file name into the groovy output console.
 		 *
 		 * @param fileName the file name
 		 */
 		public static void writeFileNameIntoConsole(String fileName) {
-			Display.getDefault().syncExec(new Runnable() {
-				@Override
-				public void run() {
-					text.setText(fileName + "\n\n");
-				}
-			});
+			if(text != null) {
+				Display.getDefault().syncExec(() -> text.setText(fileName + "\n\n"));
+			}
 		}
 
 		/**
@@ -127,12 +122,9 @@ public class GroovyOutputConsole extends ViewPart implements DBRProcessControlle
 		 * @param resultOfExecution true if this is the standard output
 		 */
 		public static void writeIntoConsole(String message, boolean resultOfExecution) {
-			Display.getDefault().syncExec(new Runnable() {
-				@Override
-				public void run() {
-					text.setText(text.getText() + message);
-				}
-			});
+			if(text != null) {
+				Display.getDefault().syncExec(() -> text.setText(text.getText() + message));
+			}
 		}
 
 		/**
@@ -141,29 +133,30 @@ public class GroovyOutputConsole extends ViewPart implements DBRProcessControlle
 		 * @param message the groovy output content
 		 */
 		public static void writeIntoConsole(String message) {
-			Display.getDefault().syncExec(new Runnable() {
-				@Override
-				public void run() {
-					if(message.equals("ERASE")){
-						text.setText("");
-					} 
-					else if (message.equals("END")) {
-						style1 = new StyleRange(text.getText().length(), message_END.length(), display.getSystemColor(SWT.COLOR_GREEN), null);
-						text.setText(text.getText() + message_END);
-						text.setStyleRange(style1);
+			if(text != null) {
+				Display.getDefault().syncExec(() -> {
+					switch (message) {
+						case "ERASE":
+							text.setText("");
+							break;
+						case "END":
+							style1 = new StyleRange(text.getText().length(), message_END.length(), display.getSystemColor(SWT.COLOR_GREEN), null);
+							text.setText(text.getText() + message_END);
+							text.setStyleRange(style1);
+							break;
+						case "BAD_END":
+							style1 = new StyleRange(text.getText().length(), message_BAD_END.length(), display.getSystemColor(SWT.COLOR_RED), null);
+							text.setText(text.getText() + message_BAD_END);
+							text.setStyleRange(style1);
+							break;
+						default:
+							text.setText(text.getText() + message);
+							break;
 					}
-					else if (message.equals("BAD_END")) {
-						style1 = new StyleRange(text.getText().length(), message_BAD_END.length(), display.getSystemColor(SWT.COLOR_RED), null);
-						text.setText(text.getText() + message_BAD_END);
-						text.setStyleRange(style1);
-					}
-					else {
-				    	text.setText(text.getText() + message);
-					}
-				}
-			});
+				});
+			}
 		}
-		
+
 	}
 
 }
